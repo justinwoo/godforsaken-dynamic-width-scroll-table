084683e833c66bc4ec5b931d8761ea65c9095f7a
Justin Woo <moomoowoo@gmail.com>
2015-05-20 21:31:31 -0500

    provide get-column-widths in the module
    just in case i actually use this module, i dont wanna be copy pasting it
    into my project or havingto provide my own implementations or something

--- example/src/index.js
+++ example/src/index.js
@@ -3,30 +3,8 @@ var Rx = require('rx');
 
 var assign = require('object-assign');
 
-var FixedScrollTable = require('../../lib/godforsaken-dynamic-width-scroll-table.js');
-
-function getColumnWidths(rowWidth, columnWidths) {
-  var computation = columnWidths.reduce(function (agg, width) {
-    if (typeof width === 'number') {
-      agg.remainingWidth -= width;
-      agg.customWidthColumns -= 1;
-    }
-    return agg;
-  }, {
-    autoSizeColumns: columnWidths.length,
-    remainingWidth: rowWidth
-  });
-
-  var standardWidth = computation.remainingWidth / computation.autoSizeColumns;
-
-  return columnWidths.map(function (width) {
-    if (width) {
-      return width;
-    } else {
-      return standardWidth;
-    }
-  });
-}
+var FixedScrollTable = require('../../lib/godforsaken-dynamic-width-scroll-table');
+var getColumnWidths = require('../../lib/get-column-widths');
 
 var ExampleTable = React.createClass({
 
